3fe0681536900429ef998cbb6abdd7d378774230
Priyanka-Patil3 <Priyanka.Patil3@ibm.com>
2019-08-19 18:52:35 +0530

    first commit

--- MyApplication/src/main/java/com/vikash/MyApplication.java
+++ MyApplication/src/main/java/com/vikash/MyApplication.java
@@ -1,9 +1,12 @@
 package com.vikash;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+
 public class MyApplication {
 
 	public static void main(String[] args) {
